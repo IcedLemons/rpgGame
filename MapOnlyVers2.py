91b333045da5b0624a9a48d13f74627ac60f3fe1
lemonbee <97649624+IcedLemons@users.noreply.github.com>
2023-03-16 03:59:41 +0000

    Changed versions

---
+++ MapOnlyVers2.py
@@ -0,0 +1,68 @@
+# ---------------------------------------------
+# Created By : Ben Chu
+# Created Date : 15/03/23
+# Version = '2.5'
+# ---------------------------------------------
+
+# Actions List, contains all possible actions by users
+actions = ["heal", "attack", "defend", "explore", "quit"]
+# Rooms list, contains all rooms accessible
+# Also contains the possible directions users can go once in said room
+rooms = [
+    ["courtyard", {"north": "Main Hall"}],
+    ["jail cells", {"east": "Main Hall"}],
+    ["throne room", {"south": "guard Hall"}],
+    ["lower Dungeons", {"west": "Main Hall"}],
+    ["guard Hall", {"north": "throne room", "south": "Main Hall"}],
+    ["Main Hall", {"north": "guard Hall", "east": "lower Dungeons", "south": "courtyard", "west": "jail cells"}]
+]
+# Sets the starting location for player
+current_location = "Main Hall"
+
+while True:
+    print("\n")
+    # Prints out the actions list
+    for action in actions:
+        print(f"- {action.capitalize()}")
+    # Checks user inout all in lowercase
+    print("\n")
+    choice = input("Select a following action: ").lower()
+    print("\n")
+    # If user input is quit then it exits the game
+    if choice == "quit":
+        print("Exiting game!")
+        break
+    # If not quit and is explored then prints out directions list within the rooms list
+    elif choice == "explore":
+        print("\nSelect a following direction:\n")
+        # Prints out current location of player
+        print(f"\nYou're currently in {current_location.capitalize()}\n")
+        # Used to find the current room player is in using the current location variable
+        # and possible directions player can go within that room
+        current_room = next((room for room in rooms if room[0] == current_location), None)
+        # IDK what this line does pls help
+        if current_room is not None:
+            # Prints out list of possible directions within the current room player is in
+            for direction in current_room[1]:
+                print(f"- {direction.capitalize()}")
+        else:
+            print("Invalid room")
+        # Checks user input within all possible directions (current_room)...? in lowercase
+        print("\n")
+        choice_direc = input("Select a direction: ").lower()
+        print("\n")
+        # IDK what this does either below
+        input_direction = next((direction for direction in current_room[1] if direction == choice_direc), None)
+        if input_direction is not None:
+            # Prints out the players location after they select a direction to go in
+            current_location = current_room[1][input_direction]
+            print(f"You are now in {current_location.capitalize()}")
+        else:
+            print("Invalid direction")
+    # If player input is neither quit nor explore and is a valid action
+    # then prints out user enacting the action
+    elif choice in actions:
+        print(f"You are now {choice.capitalize()}ing!")
+    # Error message if user input isn't in actions list
+    else:
+        print("Please select a valid action!")
---
+++ rpg_game.py
@@ -0,0 +1,52 @@
+# ---------------------------------------------
+# Created By : Ben Chu
+# Created Date : 15/03/23
+# Version = '3'
+# ---------------------------------------------
+
+actions = ["heal", "attack", "defend", "explore", "quit"]
+
+rooms = {
+    "bathroom": {"north": "livingroom"},
+    "kitchen": {"east": "livingroom"},
+    "closet": {"south": "bedroom"},
+    "basement": {"west": "livingroom"},
+    "bedroom": {"north": "closet", "south": "livingroom"},
+    "livingroom": {"north": "bedroom", "east": "basement", "south": "bathroom", "west": "kitchen"}
+}
+
+current_location = "livingroom"
+
+while True:
+    print("\n")
+  
+    for action in actions:
+        print(f"- {action.capitalize()}")
+
+    print("\n")
+    choice = input("Pick a following action: ").lower()
+    print("\n")
+
+    if choice == "quit":
+        print("Exiting game!")
+        break
+      
+    elif choice == "explore":
+        print("\nSelect a following direction:\n")
+        print(f"\nYou're currently in {current_location.capitalize()}\n")    
+        for direction in rooms[current_location]:
+            print(f"- {direction.capitalize()}")
+        print("\n")
+        choice_dir = input("Select a direction: ").lower()
+        print("\n")
+      
+        if choice_dir in rooms[current_location]:
+            current_location = rooms[current_location][choice_dir]
+            print(f"You are now in {current_location.capitalize()}")
+        else:
+            print("Invalid direction")
+          
+    elif choice in actions:
+        print(f"You are now {choice.capitalize()}ing!")
+    else:
+        print("Please select a valid action")
